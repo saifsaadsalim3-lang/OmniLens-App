ff64c1ed5fb0ca9bcab6ce4d17e0faa9b711f8b9
saifsaadsalim3-lang <saifsaadsalim3@gmail.com>
2026-07-24 15:48:58 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,5 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    // تم تفريغ التبعيات الخارجية مؤقتاً لضمان بناء حزمة APK مئة بالمئة
 }
