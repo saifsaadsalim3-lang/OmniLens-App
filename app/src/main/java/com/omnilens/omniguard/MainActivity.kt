d5123525b5009dbc1ee4daf71c3a8493e021ed54
saifsaadsalim3-lang <saifsaadsalim3@gmail.com>
2026-07-24 17:33:08 +0300

    Update MainActivity.kt

--- app/src/main/java/com/omnilens/omniguard/MainActivity.kt
+++ app/src/main/java/com/omnilens/omniguard/MainActivity.kt
@@ -1,10 +1,1 @@
-package com.omnilens.omniguard
-
-import android.app.Activity
-import android.os.Bundle
-
-class MainActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+OmniLens-App
