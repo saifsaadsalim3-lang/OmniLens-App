6061a46bcc90152018149f757e3ecaece6bb3288
saifsaadsalim3-lang <saifsaadsalim3@gmail.com>
2026-07-27 01:10:12 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,49 +1,54 @@
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace 'com.omnilens.omniguard'
-    compileSdk 34
+    namespace = "com.omnilens.omniguard"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.omnilens.omniguard"
-        minSdk 21
-        targetSdk 34
-        versionCode 19
-        versionName "1.9.1"
+        applicationId = "com.omnilens.omniguard"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 19
+        versionName = "1.9.1"
 
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    // 🎯 الكود المسؤول عن إصلاح وتحديد اسم ملف الـ APK الناتج تلقائياً
-    applicationVariants.all { variant ->
+    // تسمية ملف الـ APK الناتج تلقائياً بـ OmniLens-v1.9.1.apk
+    applicationVariants.all {
+        val variant = this
         variant.outputs.all {
-            outputFileName = "OmniLens-v${variant.versionName}.apk"
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            output.outputFileName = "OmniLens-v${variant.versionName}.apk"
         }
     }
 
     buildTypes {
-        release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    implementation 'androidx.core:core-ktx:1.12.0'
-    implementation 'androidx.appcompat:appcompat:1.6.1'
-    implementation 'com.google.android.material:material:1.11.0'
-    implementation 'androidx.activity:activity-ktx:1.8.2'
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
 }
