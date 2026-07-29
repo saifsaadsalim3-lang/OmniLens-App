8fb2c84fc8f6df59bea53221f1d03153cd698256
saifsaadsalim3-lang <saifsaadsalim3@gmail.com>
2026-07-29 08:45:48 +0300

    Update ShareMediaActivity.kt

--- app/src/main/java/com/omnilens/omniguard/ui/ShareMediaActivity.kt
+++ app/src/main/java/com/omnilens/omniguard/ui/ShareMediaActivity.kt
@@ -1,14 +1,14 @@
 package com.omnilens.omniguard.ui
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import java.io.File
 
-class ShareMediaActivity : AppCompatActivity() {
+class ShareMediaActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
